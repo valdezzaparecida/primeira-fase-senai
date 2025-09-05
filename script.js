f449dddb3a02e66697e4c939a95a955532d95ff2
valdezzaparecida <andreiavaldez31@gmail.com>
2025-09-04 21:57:46 -0300

    aula-javascript-html

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert("ola mundo!")
+// alert("ola mundo!")
+// alert("oi")
+// alert("tudo bem?")
+// alert("sovê vem sempre aqui")
+
+let idade
+
+idade = 18
+alert(idade)
+
+idade="melancia"
+alert(idade)
+
+idade = 18.1
+alert(idade)
+
+
+const amor="meu primeiro amor"
+alert(amor)
+alert(amor)
+letamor2 ="meu segundo amor"
+var antigao  = "Nunca usar essa poioca"
+
+
