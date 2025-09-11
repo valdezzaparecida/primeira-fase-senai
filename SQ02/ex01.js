1b96359cc158aa66333d43b311bd398e8fe45610
valdezzaparecida <andreiavaldez31@gmail.com>
2025-09-10 22:20:38 -0300

    exerciciossq02javaacriptrafael

---
+++ SQ02/ex01.js
@@ -0,0 +1,75 @@
+// alert("ex01 executando ok bem bom!")
+
+// let idade
+// idade= Number(prompt("Digite sua idade:"))
+
+// 
+//     alert("Maior de idade")
+//     alert("pode sair pro intervalo")
+// }else{
+//     alert("Dimenó")
+//     alert("pode sair pro recreio")
+// }
+// console.log(idade);
+
+// 1 Verificando a idade: Peça ao usuário sua idade e, em seguida, exiba uma mensagem que diga se ele é maior de idade ou menor de idade.
+
+//2 Jogo de Adivinhação: Crie um número aleatório entre 1 e 10. Peça ao usuário para adivinhar o número e, em seguida, diga se eles acertaram ou não.
+
+
+
+//3 Dia da Semana: Peça ao usuário que insira um número de 1 a 7 e, em seguida, mostre o dia da semana correspondente (por exemplo, 1 = Domingo, 2 = Segunda, etc.).
+
+
+
+//4 Verificando Números Pares: Peça ao usuário para inserir um número e, em seguida, determine se é par ou ímpar.
+
+let numeros
+numeros= Number(prompt("Digite numeros:"))
+if(numeros% 2==0 ){
+    alert("par")
+}else{
+    alert("ímpar")
+}
+
+
+
+
+
+// 5Calculadora Simples: Crie uma calculadora simples que peça ao usuário dois números e uma operação (adição, subtração, multiplicação ou divisão) e, em seguida, exiba o resultado.
+
+
+
+//6 Senha Segura: Peça ao usuário para criar uma senha. Verifique se a senha tem pelo menos 8 caracteres.
+
+
+//7 Classificação de Notas: Peça ao usuário para inserir sua nota em uma escala de 0 a 100 e, em seguida, atribua uma classificação (A, B, C, D ou E) com base na nota.
+
+
+
+//8 Comparação de Números: Peça ao usuário para inserir dois números e determine qual deles é maior.
+
+//9 Mensagem Secreta: Peça ao usuário para digitar uma senha e, se a senha for "abracadabra", exiba uma mensagem secreta.
+
+
+//10 Verificando um Ano Bissexto: Peça ao usuário para inserir um ano e determine se ele é bissexto ou não.
+
+//11 Decisão de Compra: Peça ao usuário para inserir o preço de um item e seu saldo. Em seguida, determine se eles podem comprar o item ou não.
+
+//12 Idade do Cachorro: Peça ao usuário que insira a idade de seu cachorro e, em seguida, calcule a idade do cachorro em anos humanos (um ano de cachorro equivale a sete anos humanos). Informe se o doguinho já pode se aposentar por idade :D
+
+//13 Verificando Votação: Peça ao usuário para inserir sua idade e verifique se ele é elegível para votar (idade mínima de 16 anos).
+
+//14 Verificando Letra Maiúscula: Peça ao usuário para inserir uma letra e verifique se ela é maiúscula ou minúscula.
+
+
+//15 Calculadora de IMC: Crie uma calculadora de Índice de Massa Corporal (IMC) que peça ao usuário seu peso e altura e, em seguida, calcule e exiba o IMC. Informe também em que faixa o usuário se encontra.
+
+// 16Conversor de Temperatura: Crie um conversor de temperatura que peça ao usuário para inserir uma temperatura em Celsius e, em seguida, converta-a para Fahrenheit.
+
+
+
+//17 Dias em um Mês: Peça ao usuário para inserir o nome de um mês e, em seguida, determine quantos dias ele tem.
+
+
+
