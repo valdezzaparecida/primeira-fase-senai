ed35559fa954d4d052a30e301e26f8e7dec42b7c
valdezzaparecida <andreiavaldez31@gmail.com>
2025-09-15 22:14:41 -0300

    ex5.2 terminado

--- sq5.2/ex52.js
+++ sq5.2/ex52.js
@@ -1,10 +1,10 @@
-let a = Number(window.prompt("digite um numero"))
-let b = Number(window.prompt("DIGITE OUTRO NUMERO"))
+// let a = Number(window.prompt("digite um numero"))
+// let b = Number(window.prompt("DIGITE OUTRO NUMERO"))
 
-if(a > b){ 
-    alert(`o numero ${a} é maior que ${b}`)
-}else{
-    alert(`o numro ${b} é maior que ${a}`)
-}
+// if(a > b){ 
+//     alert(`o numero ${a} é maior que ${b}`)
+// }else{
+//     alert(`o numro ${b} é maior que ${a}`)
+// }
 
-''
+// ''
