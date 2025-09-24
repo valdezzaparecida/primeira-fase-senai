0968e22ee36f96b4450d87efa9f0afa7b758e237
valdezzaparecida <andreiavaldez31@gmail.com>
2025-09-23 22:19:54 -0300

    condicionais

---
+++ logica-js/condicionais.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")()
+
+const notaFinal = prompt("Digite a nota final: ")
+const frequencia = prompt(" digite a frequência")
+
+
+
+
+if(notaFinal >= 7 && frequencia >= 8){
+
+    console.log("Aprovado")
+
+}else{
+    console.log("reprovado")
+
+
+
+}
