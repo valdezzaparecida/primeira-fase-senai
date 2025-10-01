25ad7125a56521a2cde4b613f1aeefb8fd8bf154
valdezzaparecida <andreiavaldez31@gmail.com>
2025-09-30 22:21:47 -0300

    for e Funcoes

---
+++ logica-js/for.js
@@ -0,0 +1,36 @@
+// for(let i = 0; i<=10;i++){
+// console.log(i)
+// }
+
+// for que imprima de 10 a 0
+
+// for(let i = 10; i>=0; i--){
+//     console.log(i)
+// // }
+
+
+// for(let i =2; i<=20;i+=2){
+//     console.log(i)
+// }
+
+// let soma = 0
+
+// for(let i = 1; i<100; i++){
+//     soma += i
+// }
+
+// console.log("resultado da soma",soma)
+
+// let alunos = ["João","Maria","Joana"]
+// alunos.push("Pedro")
+// for(let i=0; i<alunos.length;i++){
+
+//     console.log("alunos:", alunos[i])
+// }
+
+
+let frutas = ["maca","morango","banana","pessego","caju",]
+
+for(let i = 0;i<frutas.length;i++){
+    console.log("frutas:",frutas[i])
+}
---
+++ logica-js/funcoes.js
@@ -0,0 +1,20 @@
+// function  saudacao(nome){
+//     console.log(`Bom dia ${nome}`)
+// }
+
+// const saudacao = (nome)=>{
+//     console.log(`Bom dia, $ {nome}`)
+// }
+// saudacao("João")
+// saudacao("Maria")
+
+const soma = (x,y) => x+y //arrow function
+
+console.log(`a soma é ${3,5}`)
+// const soma = (x,y) =>{
+//     return x+y
+
+
+// }
+
+
