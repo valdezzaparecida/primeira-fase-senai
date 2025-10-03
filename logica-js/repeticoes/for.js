9ba631fb793ba9880c0040c351394aa3a62aa581
valdezzaparecida <andreiavaldez31@gmail.com>
2025-10-02 21:32:25 -0300

    for

---
+++ logica-js/repeticoes/for.js
@@ -0,0 +1,38 @@
+// let i = 0
+// while(i<3){
+//     alert(" Valor do i: " + i)
+//     i++
+// }
+// for(let i= 0; i<3;i++){
+//      alert(" Valor do i: ") +i
+
+// }
+
+// let soma = 0
+// for(let i=0; i< 10;i ++){
+//     let nota = Number (prompt("Digite a "+ (i+1) + "a nota:"))
+//     soma -soma + nota
+// }                
+
+
+// alert("Soma:" + soma)
+
+
+
+
+
+// const nomes =["Dona Bete","Mano Juca"," Lucio Fernando","Capitão ganso"]
+
+//     for(let i= 0; i<4; i++){
+//         alert(nomes[i])
+//     }
+
+
+
+
+//     let quadrado= 
+
+
+for(let i = 0; i<=10;i++){
+    alert(i)
+}for
