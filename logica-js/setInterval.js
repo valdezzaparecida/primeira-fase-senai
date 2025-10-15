8665f9e10ab40500ccf42d81286bfd920efe97d4
valdezzaparecida <andreiavaldez31@gmail.com>
2025-10-14 22:20:38 -0300

    dom

---
+++ logica-js/setInterval.js
@@ -0,0 +1,16 @@
+// setInterval(()=>{
+//     console.log("executei")
+// },2000)
+
+// const prompt= require('prompt-sync')()
+
+// let contadorInicial=Number(prompt("Digite um valor inicial: "))
+// let contadorFinal=Number(prompt("Digite um valor final:"))
+
+// let intervalo= setInterval(()=>{
+//     contadorInicial++
+//     console.log(contadorInicial)
+//     if(contadorInicial=== contadorFinal){
+//         clearInterval(intervalo)
+//     }
+// },1000)
---
+++ logica-js/setTimeout.js
@@ -0,0 +1,23 @@
+//  setTimeout(()=>{
+// console.log("Executei depois de 3seg")
+// },3000)
+
+// function saudacao(){
+//     console.log("olá")
+// }
+// function imprimeDado(){
+//     console.log("imprimir")
+// }
+// function soma(){ 
+//     console.log(1+3)
+// }
+
+// setTimeout(imprimeDado,5000)
+// saudacao()
+
+
+// setTimeout(soma,3000)
+
+
+
+
