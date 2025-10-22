d525d1f23f558d4250e1ad3ce4e85128d7633c74
valdezzaparecida <andreiavaldez31@gmail.com>
2025-10-21 22:27:19 -0300

    aula de html e css

---
+++ logica-js/repeticoes/switch.js
@@ -0,0 +1,14 @@
+// let dia = 1
+// switch (dia) {
+//     case 1:
+//         console.log("Segundou");
+//         break;
+//     case 2:
+//         console.log("Segundou")
+//         break;
+//     default:
+//         console.log("Opção Invalida")
+//         break;
+// }
+
+
