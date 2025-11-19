3d3e8999dd5e3ad15327f70276fb8479ede458aa
ANDRÉIA APARECIDA VALDEZ DA SILVA <andreia_v_da-silva@estudante.sesisenai.org.br>
2025-11-18 22:16:38 -0300

    atualizacao

--- logica-js/DOM/selecao/js/scripts.js
+++ logica-js/DOM/selecao/js/scripts.js
@@ -1,18 +1,69 @@
-// arquivo serviços
+//busca
 
-const servicoWeb = document.querySelector("#servico-web")
+const inputForm = document.querySelector("#busca")
 
-const showButton = document.querySelector("#show")
+const botaoBusca = document.querySelector("#botaoBusca")
 
-const hideButtonent= document.querySelector("#hide")
+const resultadoDiv = document.querySelector("#resultado")
 
-showButton.addEventListener("click",()=>{
-    servicoWeb .classList.add("show")
-    servicoWeb.classList.remove("hide")
-
+inputForm,addEventListener("keydown",(e)=>{
+    console.log(e.key)
 })
-hideButtonent. addEventListener("click",() =>{
-    servicoWeb.classList.add("hide")
-    servicoWeb.classList.remove("show")
-    
+
+inputForm.addEventListener("focus",(e)=>{
+    inputForm.style.backgroundColor = "#FF0"
 })
+
+inputForm.addEventListener("blur",(e)=>{
+    inputForm.style.backgroundColor = ""
+})
+
+//get e set
+
+function searchResult(){
+    const valorBusca = inputForm.value.trim()
+    if(valorBusca){
+        resultadoDiv.textContent = `Você buscou por  ${valorBusca}`
+    }else{
+        resultadoDiv.textContent = `Por favor,insira algo na busca`
+    }
+}
+
+botaoBusca.addEventListener("click",(e)=>{
+    searchResult()
+})
+
+//botão de troca de tema
+
+const botaoTrocaTema = document.querySelector("#tema")
+function alternarTema(){
+    document.body.classList.toggle("escuro")
+    document.body.classList.toggle("claro")
+}
+
+botaoTrocaTema.addEventListener("click",(e)=>{
+    alternarTema()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
